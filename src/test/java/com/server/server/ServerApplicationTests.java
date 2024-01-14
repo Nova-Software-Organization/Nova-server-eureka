94f61aad07c0607e44b399105ea75a484061e6e3
kaue de matos <kaue178hotimail@gmail.com>
2024-01-14 20:28:28 -0300

    feature:ajustes no service

--- src/test/java/com/server/server/ServerApplicationTests.java
+++ src/test/java/com/server/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.serviceregistry.serviceresgistry;
+package com.server.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +9,4 @@ class ServerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
