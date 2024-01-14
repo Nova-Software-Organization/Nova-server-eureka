a8fd4c3982e8ec4abed62c00ce8ea75c8e4b01e7
kaue de matos <kaue178hotimail@gmail.com>
2024-01-14 14:27:07 -0300

    adicionando configurações do server eureka

---
+++ src/test/java/com/serviceregistry/serviceresgistry/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceregistry.serviceresgistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
