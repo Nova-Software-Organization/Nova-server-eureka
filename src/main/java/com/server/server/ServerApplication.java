7f191b34ae23952d927689cad0899482571f8376
kaue de matos <kaue178hotimail@gmail.com>
2024-01-14 15:30:58 -0300

    fix:removendo a linha vazia

--- src/main/java/com/server/server/ServerApplication.java
+++ src/main/java/com/server/server/ServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
